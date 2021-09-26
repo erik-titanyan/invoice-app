0871b908959c931638b7aa4a0bdd2788d493b3e0
erik-titanian <erik.titanyan@inomma.com>
2021-09-27 01:08:07 +0400

    resolving CORS error

--- src/config/api/api.utils.ts
+++ src/config/api/api.utils.ts
@@ -1,7 +1,17 @@
+// below PROXY has been used for handling CORS errors
+const PROXY = "https://evening-anchorage-65916.herokuapp.com";
+
 export const handleApiRequest = async (endpoint: string): Promise<any> => {
   try {
     const res = await fetch(
-      `https://invoicesapi20210913135422.azurewebsites.net${endpoint}`
+      `${PROXY}/https://invoicesapi20210913135422.azurewebsites.net${endpoint}`,
+      {
+        headers: {
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      }
     );
     const json = await res.json();
     return json;
